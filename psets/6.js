adcf41b38cf3a297e28a6fb27e52aacc54f4a854
Arnaldo <acruzglez2000@yahoo.com>
2015-09-08 19:44:23 -0400

    Make me up to date!

--- psets/6.js
+++ psets/6.js
@@ -14,4 +14,8 @@ Look up the JavaScript string reference to find methods which may be useful!
 
 function mixUp(a, b) {
   // write your solution here...
+  var tmpA = a.substr(0,2);
+  var tmpB = b.substr(0,2);
+  return tmpB+ a.substr(2, a.length-2)+" " + tmpA + b.substr(2, b.length-2);
 }
+console.log(mixUp("Arnaldo", "Cruz"));
