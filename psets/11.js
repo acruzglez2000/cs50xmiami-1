beba419ff574cbeb710df6c99ebdb4bfe118d58d
Arnaldo <acruzglez2000@yahoo.com>
2015-09-15 17:25:30 -0400

    Pset 11

--- psets/11.js
+++ psets/11.js
@@ -11,4 +11,21 @@ Write a function named calculateAge that:
 - Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
 */
 
-// write your solution here...
+// write your solution here with two parameters...
+var calculateAge = function(birthYear, currentYear) {
+    var vege = currentYear - birthYear;
+    console.log("You are either " + (vege-1) +" or " + vege);
+};
+console.log(calculateAge(1967, 2015));
+console.log(calculateAge(1965, 2015));
+console.log(calculateAge(1970, 2015));
+
+// write your solution here with one parameters...
+var calculateAgeOne = function(birthYear) {
+    var currentYear = new Date();
+    var vege = currentYear.getFullYear() - birthYear;
+    console.log("You are either " + (vege-1) +" or " + vege);
+};
+console.log(calculateAgeOne(1967, 2015));
+console.log(calculateAgeOne(1965, 2015));
+console.log(calculateAgeOne(1970, 2015));
